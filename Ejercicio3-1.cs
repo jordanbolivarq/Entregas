678fbaad2a2d937d37df0f4044d13ea970d6c86c
Andresjales <60202369+Andresjales@users.noreply.github.com>
2020-01-28 20:51:35 -0500

    Add files via upload

---
+++ Ejercicio3-1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ejercicio_3___1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su salario: ");
+            int salario = int.Parse(Console.ReadLine());
+            int smmlv = 877803;
+
+            if (salario < (2 * smmlv))
+            {
+                Console.WriteLine("Su tarifa es la A.");
+            }
+            else if ((2 * smmlv) <= salario && salario < (4 * smmlv) )
+            {
+                Console.WriteLine("Su tarifa es la B.");
+            }
+            else
+            {
+                Console.WriteLine("Su tarifa es la C.");
+            }
+        }
+    }
+}
---
+++ Ejercicio3-2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ejercicio_3___2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su salario: ");
+            int salario = int.Parse(Console.ReadLine());
+            int smmlv = 877803;
+
+            if (salario < (2 * smmlv))
+            {
+                int cm = 3400;
+                Console.WriteLine("Su tarifa es la A y el valor de su cuota moderadora es de: " + cm);
+            }
+            else if ((2 * smmlv) <= salario && salario < (5 * smmlv))
+            {
+                int cm = 13500;
+                Console.WriteLine("Su tarifa es la B y el valor de su cuota moderadora es de: " + cm);
+            }
+            else
+            {
+                int cm = 35600;
+                Console.WriteLine("Su tarifa es la C y el valor de su cuota moderadora es de: " + cm);
+            }
+        }
+    }
+}
