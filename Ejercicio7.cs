4c796523a3a3f623b7b42306b3d77e51891be3a4
Andresjales <60202369+Andresjales@users.noreply.github.com>
2020-02-17 21:45:56 -0500

    Update Ejercicio7.cs

--- Ejercicio7.cs
+++ Ejercicio7.cs
@@ -7,50 +7,48 @@ namespace Ejercicio_7
         static void Main(string[] args)
         {
             Random aleatorio = new Random();
-            int dado = 0, total = 0, gan = 0;
+            int dado = 0, total = 0, gan = 0, turnos = 0, ant = 0;
             string continuar = "s";
 
-            if (total == 0)
-            {
-                dado = aleatorio.Next(1, 13);
-                Console.WriteLine("Sus 3 primeros lanzamientos son:  " + dado);
-                total += dado;
-                dado = aleatorio.Next(1, 13);
-                Console.WriteLine(dado);
-                total += dado;
-                dado = aleatorio.Next(1, 13);
-                Console.WriteLine(dado);
-                total += dado;
-                Console.WriteLine("Su total es: " + total);
-                Console.WriteLine("¿Desea continuar? (s/n): ");
-                continuar = Console.ReadLine();
-            }
+            dado = aleatorio.Next(1, 13);
+            Console.WriteLine("Dado =  " + dado);
+            total += dado
+            Console.WriteLine("Su total es: " + total);
+            Console.WriteLine("¿Desea continuar? (s/n): ");
+            continuar = Console.ReadLine();
+            turnos++;
+            anterior = dado;
 
             while (continuar == "s")
             {
-                dado = aleatorio.Next(10, 13);
+                anterior = dado;
+                dado = aleatorio.Next(1, 13);
                 Console.WriteLine("Dado = " + dado);
+                total += dado;
+                turnos++;
                 
-                if (dado %2 != 0)
+                if(anterior == 10 && dado == 12)
+                {
+                    Console.WriteLine("Ganaste! Conseguiste un 12 seguido de un 10.");
+                    continuar = "n";
+                }
+                else if (total >= 100)
+                {
+                    Console.WriteLine("Ganaste!");
+                    continuar = "n";
+                }
+                else if (dado %2 != 0 && turnos > 3)
                 {
                     Console.WriteLine("Sacaste un impar. Perdiste.");
                     continuar = "n";
                 }
+                
+                
                 else
                 {
-                    total += dado;
-
-                    if (total >= 100 || gan == 22)
-                    {
-                        Console.WriteLine("Ganaste!");
-                        continuar = "n";
-                    }
-                    else
-                    {
-                        Console.WriteLine("Total = " + total);
-                        Console.WriteLine("¿Deasea continuar? (s/n): ");
-                        continuar = Console.ReadLine();
-                    }
+                    Console.WriteLine("Total = " + total);
+                    Console.WriteLine("¿Deasea continuar? (s/n): ");
+                    continuar = Console.ReadLine();
                 }
             }
 
